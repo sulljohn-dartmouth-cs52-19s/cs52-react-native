5d14c636da515c29a8024e51500a3aba247a8f53
John Sullivan <sulljohn12@gmail.com>
2019-05-12 20:58:59 -0400

    Final changes

--- navigation/main_tab_bar.js
+++ navigation/main_tab_bar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import Ionicons from 'react-native-vector-icons/FontAwesome';
 import About from '../components/about';
 import SearchTab from './search_tab'
 
-const AboutTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>about</Text></View>;
-};
+// const AboutTab = (props) => {
+//   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>about</Text></View>;
+// };
 
 // const SearchTab = (props) => {
 //   return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Search</Text></View>;
